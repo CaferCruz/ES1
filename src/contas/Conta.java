4d0e36dd46ee511d14531472fe24f9ec4ed1f010
CaferCruz <cafer_cruz@id.uff.br>
2014-11-11 11:38:58 -0200

    Mudaças na classe conta e interface observer

--- src/contas/Conta.java
+++ src/contas/Conta.java
@@ -6,20 +6,37 @@
 
 package contas;
 
+import Observer.Observable;
+import Observer.Observer;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author Romulo
  */
-public abstract class Conta {
+public abstract class Conta implements Observable {
     
     private int idConta;
     private int digito;
     private String dataDeCriacao; // a data sempre terá 10 caracteres exemplo 15/03/2014
     private float saldo;
     private float taxaDeManutencao;
+    private final List<Observer> observadores;
+    
+    //---------------------------Construtor-------------------------------------
+    public Conta(int idConta, int digito, String dataDeCriacao, float saldo, float taxaDeManutencao) {
+        this.idConta = idConta;
+        this.digito = digito;
+        this.dataDeCriacao = dataDeCriacao;
+        this.saldo = saldo;
+        this.taxaDeManutencao = taxaDeManutencao;
+        observadores = new ArrayList<>();
+    }
+    
+    
     
     
     //------------------ Getter e Setters ------------------------------------
@@ -66,6 +83,25 @@ public abstract class Conta {
         out.writeUTF(dataDeCriacao);
         out.writeFloat(saldo);
         out.writeFloat(taxaDeManutencao);
+    }
+
+    //-------------------------Operações de Observable------------------------------------
+    
+    @Override
+    public void addObserver(Observer o) {
+        observadores.add(o);
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+        observadores.remove(o);
+    }
+
+    @Override
+    public void notifyObserver() {
+        for(Observer o : observadores){
+            o.update();
+        }
     }
     
 }
